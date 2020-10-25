92a0dac53fa42b678ed96ef0f29d99d1ddea5c38
Gabriel Silva <gabrielsos99@gmail.com>
2020-10-25 04:30:47 -0300

    feat: added new field on database

--- src/database/migrations/20201001215227_createUser.ts
+++ src/database/migrations/20201001215227_createUser.ts
@@ -6,6 +6,7 @@ export async function up(knex: Knex): Promise<void> {
     table.string('name').notNullable();
     table.string('email').notNullable();
     table.string('password').notNullable();
+    table.integer('passwordExpired').notNullable();
     table.integer('userType').notNullable();
   });
 }
