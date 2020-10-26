9603ef9fde224d97b65f78719ec65b59a69e72c6
Gabriel Silva <gabrielsos99@gmail.com>
2020-10-25 23:37:21 -0300

    doc: tsc build

--- dist/src/server.js
+++ dist/src/server.js
@@ -4,12 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+require("dotenv/config");
 var cors_1 = __importDefault(require("cors"));
 var routes_1 = __importDefault(require("./routes"));
 var app = express_1.default();
 app.use(cors_1.default());
 app.use(express_1.default.json());
 app.use(routes_1.default);
-app.listen(3333, function () {
+app.listen(process.env.PORT || 3333, function () {
     console.log('server started!');
 });
